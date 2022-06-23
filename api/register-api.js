1a2e0d2f6383942e54fa80857eafb3eaa72f2d60
Christiano112 <94331754+Christiano112@users.noreply.github.com>
2022-06-23 22:59:06 +0100

    Added validation to the login page

--- api/register-api.js
+++ api/register-api.js
@@ -1,38 +1,70 @@
 // Form Validation
 document.getElementById("form").addEventListener("submit", function(a) {
     a.preventDefault();
-    let pass1 = document.getElementById("password").value;
-    let pass2 = document.getElementById("confirm_password").value
-    if (pass1 !== pass2) {
-        document.getElementById("check_password").innerHTML = "Password doesn't match, Try again!"
-    } else {
-        // API CALL
+
+    var name = document.forms.form.name.value;
+    var username = document.forms.form.username.value;
+    var email = document.forms.form.email.value;
+    var password = document.forms.form.password.value;
+    var confirmPassword = document.forms.form.confirm_password.value;
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/g;
+    var regUser = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+    var regName = /\d+$/g;
+
+    if (name == "" || regName.test(name)) {
+        document.getElementById("check_name").innerHTML = "Name cannot contain any number!";
+        // name.focus();
+        return false;
+    }
+
+    if (username == "" || !regUser.test(username)) {
+        document.getElementById("check_username").innerHTML = "Invalid username, Try another!";
+        // username.focus();
+        return false;
+    }
+
+    if (email == "" || !regEmail.test(email)) {
+        document.getElementById("check_email").innerHTML = "Enter a valid email";
+        // email.focus();
+        return false;
+    }
+
+    if (password == "" || password.length < 6) {
+        document.getElementById("check_password1").innerHTML = "Password must be at least 6 characters"
+        // password.focus();
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        document.getElementById("check_password").innerHTML = "Password doesn't match, Try again!";
+        // confirmPassword.focus();
+        return false;
+    }
+    
+    // API CALL
+    else {
         const form = document.getElementById("formr")
 
-        form.addEventListener("submit", function (e) {
-            e.preventDefault();
+        const prePayload = new FormData(form);
+        const payload = new URLSearchParams(prePayload);
 
-            const prePayload = new FormData(form);
-            const payload = new URLSearchParams(prePayload);
-
-            console.log([...payload]);
+        console.log([...payload]);
 
 
-            fetch('https://todo22a.herokuapp.com/api/v1/user/register', {
-                method: 'POST',
-                body: payload,
-                redirect: 'follow'
-            })
-                .then(res => res.json())
-                .then(data => console.log(data))
-                .catch(err => console.log('err', err));
-
-
-            setTimeout(openNew, 3000)
-            function openNew() {
-                alert("Registration is Successful");
-                window.open("home.html", target = "_self")
-            }
+        fetch('https://todo22a.herokuapp.com/api/v1/user/register', {
+            method: 'POST',
+            body: payload,
+            redirect: 'follow'
         })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log('err', err));
+
+
+        setTimeout(openNew, 3000)
+        function openNew() {
+            alert("Registration is Successful");
+            window.open("home.html", target = "_self")
+        }
     }
 })
