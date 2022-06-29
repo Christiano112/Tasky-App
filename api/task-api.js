2889ccc772d3e86b4733723eed8d595f72ced368
Christiano112 <94331754+Christiano112@users.noreply.github.com>
2022-06-29 09:48:12 +0100

    Added changes

--- api/task-api.js
+++ api/task-api.js
@@ -1,30 +1,30 @@
-// let newTask = document.querySelector("#add-task");
+let newTask = document.querySelector("#add-task");
 
-// newTask.addEventListener("click", function() {
+newTask.addEventListener("click", function() {
 
-//     var raw = "{\r\n    \r\n   \"user_id\":\"184\",\r\n	\"todo\": \"hungry need to eat\"\r\n	\r\n    \r\n}";
+    // var raw = "{\r\n    \r\n   \"user_id\":\"184\",\r\n	\"todo\": \"hungry need to eat\"\r\n	\r\n    \r\n}";
 
-//     var requestOptions = {
-//         method: 'POST',
-//         body: raw,
-//         redirect: 'follow'
-//     };
+    var requestOptions = {
+        method: 'POST',
+        body: raw,
+        redirect: 'follow'
+    };
 
-//     fetch("https://todo22a.herokuapp.com/api/v1/task", requestOptions)
-//         .then(response => response.text())
-//         .then(result => console.log(result))
-//         .catch(error => console.log('error', error));
-// })
-
-
+    fetch("https://todo22a.herokuapp.com/api/v1/task", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+})
 
 
-// var requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow'
-// };
 
-// fetch("https://todo22a.herokuapp.com/api/v1/task/", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch("https://todo22a.herokuapp.com/api/v1/task/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
