54b9ee3d178fe04e6d911d7be6d6dff30b127b04
Christiano112 <94331754+Christiano112@users.noreply.github.com>
2022-07-03 23:02:30 +0100

    made chnages

--- api/task-api.js
+++ api/task-api.js
@@ -4,9 +4,16 @@ newTask.addEventListener("click", function() {
 
     // var raw = "{\r\n    \r\n   \"user_id\":\"184\",\r\n	\"todo\": \"hungry need to eat\"\r\n	\r\n    \r\n}";
 
+    const form = document.getElementById("info")
+
+    const prePayload = new FormData(form);
+    const payload = new URLSearchParams(prePayload);
+
+    console.log([...payload]);
+
     var requestOptions = {
         method: 'POST',
-        body: raw,
+        body: payload,
         redirect: 'follow'
     };
 
@@ -19,12 +26,27 @@ newTask.addEventListener("click", function() {
 
 
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+// };
 
-fetch("https://todo22a.herokuapp.com/api/v1/task/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+// fetch("https://todo22a.herokuapp.com/api/v1/task/", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+
+
+// ALERT TO NOTIFY YOU OF YOUR TASKS
+let myDate = document.getElementById("date").value;
+
+let d = new Date();
+let secondDate = d.toISOString();
+
+console.log(secondDate);
+
+if (secondDate == myDate) {
+    alert("You have a Task to Complete");
+}
+
+// ['date', '2022-07-03T22:37']
